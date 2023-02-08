881e8836635f2773f9afe7d6940b502210c26cd9
hmix13 <hmix13@gmail.com>
2023-02-08 14:59:58 +0530

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import openai
+
+# Get API key for OpenAI
+#secrets = openai_secret_manager.get_secrets("openai")
+openai.api_key = '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'
+
+# Use the OpenAI API to analyze the sentiment of text
+@st.cache(allow_output_mutation=True)
+def analyze_sentiment(prompt):
+    import openai
+    openai.api_key = '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'
+
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message
+
+# Create the Streamlit app
+st.set_page_config(page_title="Sentiment Analyzer", page_icon=":guardsman:", layout="wide")
+st.title("Sentiment Analyzer")
+
+# Get user input
+text = st.text_area("Enter the text you want to analyze:")
+
+# Analyze sentiment
+if text:
+    sentiment = analyze_sentiment(f"Analyze the sentiment of: {text}")
+    st.success(sentiment)
